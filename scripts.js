06654caa55839b386eab86c19afab275ad1a6675
Jefferey Huang <jeffereyhuang@gmail.com>
2017-01-29 16:46:36 -0500

    major updates

--- scripts.js
+++ scripts.js
@@ -1,74 +1,38 @@
 $(function(){
 
-	// Input gains focus on document ready
-	//$('input').focus();
+	// initializes money spent
   var $total = 0;
   document.getElementById("total").innerHTML = $total;
-	// Define function to add tasks to list
-	function addTodo(){
-		if($('#money').val().trim() == "") {
-			// Show error message
-			$('.error').fadeIn(200);
-			} else {
-			// Hide error message
-			$('.error').hide();
 
-			// Get input value
-			$money =  parseInt(document.getElementById("money").value, 10);
-      if (isNaN($money)) {
-         //error
-         $('.error').show();
-          addTodo();
-       }
-      $total = $money + $total;
-       document.getElementById("total").innerHTML = $total;
+	// Define function to add transactions/update money
+	function logTransaction(){
+		//checks money
+		$money =  parseInt(document.getElementById("money").value, 10);
+    if (isNaN($money)) {
+       //don't log
+       logTransaction();
+     }
+    $total = $money + $total;
+    document.getElementById("total").innerHTML = $total;
 
-			// Create new list item
-			var $newListItem = $('<li class="todo">' + $text + '<span>' +
-			'<a href="#" class="check">' + '<i class="fa fa-check fa-lg"></i>' + '</a>' +
-			'<a href="#" class="delete-todo">' + '<i class="fa fa-trash-o fa-lg"></i>' + '</a>' +
-			'</span>' + '</li>');
+		// Create new list item
+		var $newListItem = $('<li class="todo">' + $money + '</li>');
 
-			// Add list item to end of list
-			var $addListItem = $('ul').append($newListItem);
+		// Add list item to end of list
+		var $addListItem = $('ul').append($newListItem);
 
 
-			// Hide list item before fading it into view
-			$newListItem.hide().fadeIn(500);
+		// Hide list item before fading it into view
+		$newListItem.hide().fadeIn(100);
 
-			// Refocus input box for next task
-			$('input').val("").focus();
-		}
 	};
 
-	// Clear error message when 'x' icon is clicked
-	$('i.fa-times').on('click', function(){
-		$('.error').hide();
-	});
 
-	// Call addTodo function on click
-	$('.add-todo').on('click', addTodo);
-
-
-	// Call addTodo function when enter key is pressed
+	// Call logTransaction function when enter key is pressed
 	$(document).on('keypress', function(e){
 		if(e.which == 13) {
-			addTodo();
+			logTransaction();
 		}
-	});
-
-
-	// Clear typed text and refocus input box
-	$('.clear-text').on('click', function(e) {
-		e.preventDefault();
-		$('input').val("").focus();
-	});
-
-
-	// Mark list item complete
-	$(document).on('click', '.check', function(e){
-		e.preventDefault();
-		$(this).closest('li').toggleClass('complete');
 	});
 
 
@@ -79,6 +43,5 @@ $(function(){
 			$(this).remove();
 		});
 	});
-  // var $total = $total - $(this).
-  // alert("the result is" + $total);
+
 });
